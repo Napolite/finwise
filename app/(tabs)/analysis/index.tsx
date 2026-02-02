128f646d5678e95aebcf8f7afbf9feb06ac40126
Constantine <Okonemmanuelmma@gmail.com>
2026-02-02 15:19:51 +0100

    aaded balance report

--- app/(tabs)/analysis/index.tsx
+++ app/(tabs)/analysis/index.tsx
@@ -22,6 +22,7 @@ const Analysis = () => {
           paddingTop: 40,
         }}
       >
+        {/* top tab thingy */}
         <View className="w-[80%] flex flex-row items-center p-[10px] justify-between bg-[#DFF7E2] h-[61px] rounded-[20px] mt-[20px] mx-auto">
           <TouchableOpacity
             className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
@@ -56,6 +57,7 @@ const Analysis = () => {
             <Text className="font-bold">Yearly</Text>
           </TouchableOpacity>
         </View>
+        {/* Graoh component */}
         <View className="w-[80%] max-h-[257px] bg-[#DFF7E2] mx-auto mt-[30px] rounded-[30px] py-[10px] px-[20px]">
           <View className="mb-[20px] flex justify-between flex-row items-center">
             <Text className="text-[16px] font-bold">Income & Expenses</Text>
@@ -69,6 +71,34 @@ const Analysis = () => {
             </View>
           </View>
           <AnalysisGraph />
+        </View>
+
+        {/* other side */}
+
+        <View className="flex flex-row w-[80%] justify-between mx-auto mt-[50px] px-[10px]">
+          <View>
+            <View className="flex  gap-x-[10px] items-center">
+              <View className="border border-[3px] border-[#00D09E] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
+                <Feather name="arrow-up-right" size={17} color="#00D09E" />
+              </View>
+              <Text className="text-[16px]">Total Balance</Text>
+            </View>
+            <Text className="text-[32px] font-bold text-[#000000]">
+              $7,783.00
+            </Text>
+          </View>
+          <View className="w-[2px] h-full bg-[#ffffff]" />
+          <View>
+            <View className="flex w gap-x-[10px] items-center">
+              <View className="border border-[3px] border-[#0068FF] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
+                <Feather name="arrow-down-right" size={17} color="#0068FF" />
+              </View>
+              <Text className="text-[16px]">Total Expenses</Text>
+            </View>
+            <Text className="text-[32px] font-bold text-[#0068FF]">
+              -$1.187.40
+            </Text>
+          </View>
         </View>
       </View>
     </View>
