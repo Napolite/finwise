e910e586b3959005c451b1b4b9df667b3e463b9a
Constantine <Okonemmanuelmma@gmail.com>
2026-02-04 22:37:40 +0100

    added thos image sha

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,10 +1,26 @@
+import Header from "@/components/header";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="bg-[#00D09E] min-h-full">
+      <Header pageTitle="Profile" />
+      <View
+        className="bg-[#ffffff] min-h-full w-full rounded-t-full"
+        style={{
+          borderTopLeftRadius: 80,
+          borderTopRightRadius: 80,
+          marginTop: 40,
+          paddingTop: 40,
+          paddingBottom: 40,
+          height: "100%",
+        }}
+      >
+        <View>
+          <Profile />
+        </View>
+      </View>
     </View>
   );
 };
---
+++ components/profile/profile.tsx
@@ -0,0 +1,13 @@
+import { Image } from "expo-image";
+import React from "react";
+import { View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View>
+      <Image source={"@/assets/images/ava.png"} />
+    </View>
+  );
+};
+
+export default Profile;
