db8a782755670842a7f1c00b1c85cc22467af4ce
Constantine <Okonemmanuelmma@gmail.com>
2026-02-11 14:06:15 +0100

    added auickanalysis page

--- app/(tabs)/analysis/index.tsx
+++ app/(tabs)/analysis/index.tsx
@@ -9,145 +9,155 @@ import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 const Analysis = () => {
   const [tab, setTab] = useState("d");
   return (
-    <ScrollView className="bg-[#00D09E] h-full flex-1">
+    <View className="bg-[#00D09E] h-full flex-1">
       <View>
         <Header pageTitle={"Analysis"} />
       </View>
-      <BalanceStatement />
-      <View
-        className="flex-1 bg-[#ffffff] min-h-full w-full rounded-t-full"
-        style={{
-          borderTopLeftRadius: 80,
-          borderTopRightRadius: 80,
-          marginTop: 40,
-          paddingTop: 40,
-        }}
-      >
-        {/* top tab thingy */}
-        <View className="w-[80%] flex flex-row items-center p-[10px] justify-between bg-[#DFF7E2] h-[61px] rounded-[20px] mt-[20px] mx-auto">
-          <TouchableOpacity
-            className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
-            style={{ backgroundColor: tab === "d" ? "#00d09e" : "transparent" }}
-            onPress={() => setTab("d")}
-          >
-            <Text className="font-bold">Daily</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
-            style={{ backgroundColor: tab === "w" ? "#00d09e" : "transparent" }}
-            onPress={() => setTab("w")}
-          >
-            <Text className="font-bold">Weekly</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
-            style={{ backgroundColor: tab === "m" ? "#00d09e" : "transparent" }}
-            onPress={() => {
-              setTab("m");
-            }}
-          >
-            <Text className="font-bold">Monthly</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
-            style={{ backgroundColor: tab === "y" ? "#00d09e" : "transparent" }}
-            onPress={() => {
-              setTab("y");
-            }}
-          >
-            <Text className="font-bold">Yearly</Text>
-          </TouchableOpacity>
-        </View>
-        {/* Graoh component */}
-        <View className="w-[80%] max-h-[257px] bg-[#DFF7E2] mx-auto mt-[30px] rounded-[30px] py-[10px] px-[20px]">
-          <View className="mb-[20px] flex justify-between flex-row items-center">
-            <Text className="text-[16px] font-bold">Income & Expenses</Text>
-            <View className="flex justify-between items-center flex-row gap-x-[20px]">
-              <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
-                <Feather name="search" size={20} color="black" />
-              </View>
-              <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
-                <Feather name="calendar" size={20} color="black" />
+      <ScrollView>
+        <BalanceStatement />
+        <View
+          className="flex-1 bg-[#ffffff] min-h-full w-full rounded-t-full"
+          style={{
+            borderTopLeftRadius: 80,
+            borderTopRightRadius: 80,
+            marginTop: 40,
+            paddingTop: 40,
+          }}
+        >
+          {/* top tab thingy */}
+          <View className="w-[80%] flex flex-row items-center p-[10px] justify-between bg-[#DFF7E2] h-[61px] rounded-[20px] mt-[20px] mx-auto">
+            <TouchableOpacity
+              className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
+              style={{
+                backgroundColor: tab === "d" ? "#00d09e" : "transparent",
+              }}
+              onPress={() => setTab("d")}
+            >
+              <Text className="font-bold">Daily</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
+              style={{
+                backgroundColor: tab === "w" ? "#00d09e" : "transparent",
+              }}
+              onPress={() => setTab("w")}
+            >
+              <Text className="font-bold">Weekly</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
+              style={{
+                backgroundColor: tab === "m" ? "#00d09e" : "transparent",
+              }}
+              onPress={() => {
+                setTab("m");
+              }}
+            >
+              <Text className="font-bold">Monthly</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="h-full w-[23%] rounded-[10px] flex flex-row justify-center items-center"
+              style={{
+                backgroundColor: tab === "y" ? "#00d09e" : "transparent",
+              }}
+              onPress={() => {
+                setTab("y");
+              }}
+            >
+              <Text className="font-bold">Yearly</Text>
+            </TouchableOpacity>
+          </View>
+          {/* Graoh component */}
+          <View className="w-[80%] max-h-[257px] bg-[#DFF7E2] mx-auto mt-[30px] rounded-[30px] py-[10px] px-[20px]">
+            <View className="mb-[20px] flex justify-between flex-row items-center">
+              <Text className="text-[16px] font-bold">Income & Expenses</Text>
+              <View className="flex justify-between items-center flex-row gap-x-[20px]">
+                <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
+                  <Feather name="search" size={20} color="black" />
+                </View>
+                <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
+                  <Feather name="calendar" size={20} color="black" />
+                </View>
               </View>
             </View>
+            <AnalysisGraph />
           </View>
-          <AnalysisGraph />
-        </View>
 
-        {/* other side */}
+          {/* other side */}
 
-        <View className="flex flex-row w-[70%] justify-between mx-auto mt-[50px] px-[10px]">
-          <View>
-            <View className="flex  gap-x-[10px] items-center">
-              <View className="border border-[3px] border-[#00D09E] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
-                <Feather name="arrow-up-right" size={17} color="#00D09E" />
+          <View className="flex flex-row w-[70%] justify-between mx-auto mt-[50px] px-[10px]">
+            <View>
+              <View className="flex  gap-x-[10px] items-center">
+                <View className="border border-[3px] border-[#00D09E] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
+                  <Feather name="arrow-up-right" size={17} color="#00D09E" />
+                </View>
+                <Text className="text-[16px]">Total Balance</Text>
               </View>
-              <Text className="text-[16px]">Total Balance</Text>
-            </View>
-            <Text className="text-[24px] font-bold text-[#000000]">
-              $7,783.00
-            </Text>
-          </View>
-          <View className="w-[2px] h-full bg-[#ffffff]" />
-          <View>
-            <View className="flex w gap-x-[10px] items-center">
-              <View className="border border-[3px] border-[#0068FF] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
-                <Feather name="arrow-down-right" size={17} color="#0068FF" />
-              </View>
-              <Text className="text-[16px]">Total Expenses</Text>
-            </View>
-            <Text className="text-[24px] font-bold text-[#0068FF]">
-              -$1.187.40
-            </Text>
-          </View>
-        </View>
-
-        {/*my targets */}
-        <View className="w-[80%] mx-auto mt-[30px]">
-          <Text className="text-[18px] font-bold mb-[30px]">My Targets</Text>
-          <View className="h-fit w-full flex-1 mb-[100px] flex flex-row justify-between">
-            <View className="bg-[#6DB6FE] w-[47%] h-[167px] rounded-[50px] flex items-center justify-center">
-              <Circular
-                size={103}
-                progress={0.3}
-                fill1={"#0068FF"}
-                fill2={"#0068FF"}
-                id={"size1"}
-                strokeWidth={5}
-                track={true}
-                trackStroke=""
-              >
-                <Text className="text-[#F1FFF3] text-[20px] font-bold">
-                  30%
-                </Text>
-              </Circular>
-              <Text className="text-[#F1FFF3] text-[16px] font-bold mt-[10px]">
-                Travel
+              <Text className="text-[24px] font-bold text-[#000000]">
+                $7,783.00
               </Text>
             </View>
-            <View className="bg-[#6DB6FE] w-[47%] h-[167px] rounded-[50px] flex items-center justify-center">
-              <Circular
-                size={103}
-                progress={0.5}
-                fill1={"#0068FF"}
-                fill2={"#0068FF"}
-                id={"size1"}
-                strokeWidth={5}
-                track={true}
-                trackStroke=""
-              >
-                <Text className="text-[#F1FFF3] text-[20px] font-bold">
-                  50%
-                </Text>
-              </Circular>
-              <Text className="text-[#F1FFF3] text-[16px] font-bold mt-[10px]">
-                Car
+            <View className="w-[2px] h-full bg-[#ffffff]" />
+            <View>
+              <View className="flex w gap-x-[10px] items-center">
+                <View className="border border-[3px] border-[#0068FF] border-solid w-[25px] h-[25px] flex flex-row items-center justify-center rounded-[5px]">
+                  <Feather name="arrow-down-right" size={17} color="#0068FF" />
+                </View>
+                <Text className="text-[16px]">Total Expenses</Text>
+              </View>
+              <Text className="text-[24px] font-bold text-[#0068FF]">
+                -$1.187.40
               </Text>
             </View>
           </View>
+
+          {/*my targets */}
+          <View className="w-[80%] mx-auto mt-[30px]">
+            <Text className="text-[18px] font-bold mb-[30px]">My Targets</Text>
+            <View className="h-fit w-full flex-1 mb-[100px] flex flex-row justify-between">
+              <View className="bg-[#6DB6FE] w-[47%] h-[167px] rounded-[50px] flex items-center justify-center">
+                <Circular
+                  size={103}
+                  progress={0.3}
+                  fill1={"#0068FF"}
+                  fill2={"#0068FF"}
+                  id={"size1"}
+                  strokeWidth={5}
+                  track={true}
+                  trackStroke=""
+                >
+                  <Text className="text-[#F1FFF3] text-[20px] font-bold">
+                    30%
+                  </Text>
+                </Circular>
+                <Text className="text-[#F1FFF3] text-[16px] font-bold mt-[10px]">
+                  Travel
+                </Text>
+              </View>
+              <View className="bg-[#6DB6FE] w-[47%] h-[167px] rounded-[50px] flex items-center justify-center">
+                <Circular
+                  size={103}
+                  progress={0.5}
+                  fill1={"#0068FF"}
+                  fill2={"#0068FF"}
+                  id={"size1"}
+                  strokeWidth={5}
+                  track={true}
+                  trackStroke=""
+                >
+                  <Text className="text-[#F1FFF3] text-[20px] font-bold">
+                    50%
+                  </Text>
+                </Circular>
+                <Text className="text-[#F1FFF3] text-[16px] font-bold mt-[10px]">
+                  Car
+                </Text>
+              </View>
+            </View>
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 };
 
--- screen-components/home-components/quickAnalysis.tsx
+++ screen-components/home-components/quickAnalysis.tsx
@@ -1,49 +1,92 @@
-import BalanceStatement from "@/components/balanceStatement";
+import Circular from "@/components/circularProgress";
 import Header from "@/components/header";
 import Table from "@/components/table";
 import AnalysisGraph from "@/screen-components/analysis-component/analysis-graph";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import Feather from "@expo/vector-icons/Feather";
 import React from "react";
 import { Animated, ScrollView, Text, View } from "react-native";
+
 import { transactions } from "./expenses";
 
 const QuickAnalysis = ({ setShowQAnalysis }: any) => {
   return (
-    <ScrollView className="bg-[#00D09E] h-full flex-1">
+    <View className="bg-[#00D09E] h-full flex-1">
       <View>
         <Header pageTitle={"Quick Analysis"} backAction={setShowQAnalysis} />
       </View>
-      <BalanceStatement />
-      <View
-        className="flex-1 bg-[#ffffff] min-h-full w-full rounded-t-full"
-        style={{
-          borderTopLeftRadius: 80,
-          borderTopRightRadius: 80,
-          marginTop: 40,
-          paddingTop: 40,
-        }}
-      >
-        {/* Graoh component */}
-        <View className="w-[80%] max-h-[257px] bg-[#DFF7E2] mx-auto mt-[30px] rounded-[30px] py-[10px] px-[20px]">
-          <View className="mb-[20px] flex justify-between flex-row items-center">
-            <Text className="text-[16px] font-bold">April Expenses</Text>
-            <View className="flex justify-between items-center flex-row gap-x-[20px]">
-              <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
-                <Feather name="search" size={20} color="black" />
+      <ScrollView>
+        <View
+          className="w-[90%] flex flex-row justify-between items-center  h-[15vh] bg-[#00D09E] mx-auto px-[10px] rounded-[20px] py-[10px] mt-[50px]"
+          style={[{ width: 357 }]}
+        >
+          <View className="flex items-center w-[30%]">
+            <Circular
+              size={95}
+              progress={0.5}
+              fill1={"#0068FF"}
+              fill2={"#0068FF"}
+              id={"size1"}
+              strokeWidth={5}
+              track={true}
+            >
+              <AntDesign name="car" size={52} color="black" />
+            </Circular>
+            <Text className="text-[14px] font-bold w-[63px] text-center mt-[10px]">
+              Savings on Goals
+            </Text>
+          </View>
+          <View className="w-[2px] h-full bg-[#ffffff]" />
+          <View className="flex justify-between h-full w-[60%] items-center">
+            <View className="flex flex-row items-center gap-x-[20px] flex-1">
+              <FontAwesome name="money" size={34} color="black" />
+              <View>
+                <Text>Revenue last week</Text>
+                <Text className="font-bold text-[16.8px]">$4,000</Text>
               </View>
-              <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
-                <Feather name="calendar" size={20} color="black" />
+            </View>
+            <View className="w-full h-[2px] bg-[#ffffff]" />
+
+            <View className="flex flex-row items-center gap-x-[20px] flex-1">
+              <FontAwesome name="cutlery" size={27} color="black" />
+              <View>
+                <Text>Revenue last week</Text>
+                <Text className="font-bold text-[16.8px]">$4,000</Text>
               </View>
             </View>
           </View>
-          <AnalysisGraph />
         </View>
-        {/* Table */}
-        <Animated.View className="mb-[80px] mt-[50px]">
-          <Table data={[...transactions]} handleScrollAction={() => {}} />
-        </Animated.View>
-      </View>
-    </ScrollView>
+        <View
+          className="flex-1 bg-[#ffffff] min-h-full w-full rounded-t-full"
+          style={{
+            borderTopLeftRadius: 80,
+            borderTopRightRadius: 80,
+            marginTop: 40,
+            paddingTop: 40,
+          }}
+        >
+          {/* Graoh component */}
+          <View className="w-[80%] max-h-[257px] bg-[#DFF7E2] mx-auto mt-[30px] rounded-[30px] py-[10px] px-[20px]">
+            <View className="mb-[20px] flex justify-between flex-row items-center">
+              <Text className="text-[16px] font-bold">April Expenses</Text>
+              <View className="flex justify-between items-center flex-row gap-x-[20px]">
+                <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
+                  <Feather name="search" size={20} color="black" />
+                </View>
+                <View className="h-[32px] w-[32px] rounded-[10px] bg-dgreen items-center justify-center flex">
+                  <Feather name="calendar" size={20} color="black" />
+                </View>
+              </View>
+            </View>
+            <AnalysisGraph />
+          </View>
+          {/* Table */}
+          <Animated.View className="mb-[80px] mt-[50px]">
+            <Table data={[...transactions]} handleScrollAction={() => {}} />
+          </Animated.View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
